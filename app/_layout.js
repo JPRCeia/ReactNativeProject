7746b94c2f23ee35752b37e2e041e24fb0de5680
joao ramos <joaopramos076@gmail.com>
2024-09-08 21:38:54 -0300

    Welcome hudle

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,27 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import* as SplashScreen from 'expo-splash-screen';
 
 const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        'DMSans-Bold': require('../assets/fonts/DMSans-Bold.ttf'),
+        'DMSans-Medium': require('../assets/fonts/DMSans-Medium.ttf'),
+        'DMSans-Regular': require('../assets/fonts/DMSans-Regular.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+           await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <Stack onLayout={onLayoutRootView} />;
+    
     return <Stack />;
 };
 
