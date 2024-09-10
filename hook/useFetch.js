00dcb1079f2ca0e26cf73c2ded9acc10f6e6abdd
Joao Ramos <joao.ramos01@sap.com>
2024-09-10 10:05:03 -0300

    Hook for API

---
+++ hook/useFetch.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = () => {
+  const [data, setData] = setState([]);
+  const [isLoading, setIsLoading] = setState(false);
+  const [error, setError] = setState(null);
+};
